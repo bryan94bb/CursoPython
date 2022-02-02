06e790f03ee214a6e8c507208e933d8dd15e7531
Bryan Buenano <ismael.-94@hotmail.com>
2022-02-01 19:00:23 -0500

    inicio

---
+++ prueba.py
@@ -0,0 +1,14 @@
+#Comentario en una linea?
+'''
+Comentario
+en
+Varias 
+Lineas
+'''
+#La entrada de datos
+#input()
+#La  salida de datos
+print("Formulario")
+print("Ingresa el nombre: ")
+Nombre = input()
+print("El nombre que ingresaste es", Nombre)
